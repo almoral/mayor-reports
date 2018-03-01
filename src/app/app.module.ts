7af8d401be4c1d5d0ad069007dcc181a12ba4111
Alejandro Morales <alexmorales30@gmail.com>
2018-02-28 22:00:19 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { SearchBoxComponent } from './search-box';
+import { ResetButtonComponent } from './reset-button';
 import { AppComponent } from './app.component';
+import {MonthSelectorComponent} from './month-selector';
+import {YearSelectorComponent} from './year-selector';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent,
+    ResetButtonComponent,
+    MonthSelectorComponent,
+    YearSelectorComponent
   ],
   imports: [
     BrowserModule
