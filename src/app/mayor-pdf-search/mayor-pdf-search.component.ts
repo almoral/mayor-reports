db27ccf9a748fe766cb7cf517f94542f2472cb50
Alex Morales <almoral>
2019-04-09 16:02:33 -0400

    WIP: Getting month, year and text filters working together.

--- src/app/mayor-pdf-search/mayor-pdf-search.component.ts
+++ src/app/mayor-pdf-search/mayor-pdf-search.component.ts
@@ -7,72 +7,97 @@ import { MonthService } from '../shared/services/month.service';
 import * as _ from 'lodash';
 
 @Component({
-    selector: 'mdc-mayor-pdf-search',
-    templateUrl: './mayor-pdf-search.component.html',
-    styleUrls: [ './mayor-pdf-search.component.css' ]
+  selector: 'mdc-mayor-pdf-search',
+  templateUrl: './mayor-pdf-search.component.html',
+  styleUrls: ['./mayor-pdf-search.component.css']
 })
 export class MayorPdfSearchComponent implements OnInit {
-    files$: Observable<object>;
-    yearsSubject = new BehaviorSubject<object[]>([]);
-    years$ = this.yearsSubject.asObservable();
+  files$: Observable<object>;
+  yearsSubject = new BehaviorSubject<object[]>([]);
+  years$ = this.yearsSubject.asObservable();
 
-    currentSelectedYears$: Observable<string[]>;
-    currentSelectedMonths$: Observable<string[]>;
+  currentSelectedYears$: Observable<string[]>;
+  currentSelectedMonths$: Observable<string[]>;
 
-    months = this.monthService.months;
+  months = this.monthService.months;
 
-    constructor(
-        private documentService: DocStoreService,
-        private ngxService: NgxUiLoaderService,
-        private monthService: MonthService
-    ) {}
+  constructor(
+    private documentService: DocStoreService,
+    private ngxService: NgxUiLoaderService,
+    private monthService: MonthService
+  ) {}
 
-    ngOnInit() {
-        this.ngxService.start();
+  ngOnInit() {
+    this.ngxService.start();
 
-        this.currentSelectedYears$ = this.documentService.currentSelectedYears$;
-        this.currentSelectedMonths$ = this.documentService.currentSelectedMonths$;
+    this.currentSelectedYears$ = this.documentService.currentSelectedYears$;
+    this.currentSelectedMonths$ = this.documentService.currentSelectedMonths$;
 
-        this.files$ = this.documentService.filteredDocuments$.pipe(
-            tap((data: object[]) => {
-                // console.log('data: ', data);
-                if (!_.isNil(data) && data.length > 0) {
-                    this.ngxService.stop();
-                    this.getYearsFromResults(data);
-                }
-            })
-        );
-    }
+    this.files$ = this.documentService.filteredDocuments$.pipe(
+      tap((data: object[]) => {
+        // console.log('data: ', data);
+        if (!_.isNil(data) && data.length > 0) {
+          this.ngxService.stop();
+          this.getYearsFromResults(data);
+        }
+      })
+    );
 
-    getYearsFromResults(data: object[]) {
-        of(data)
-            .pipe(
-                map((files) => {
-                    const years = _.compact(_.uniq(_.map(files, 'year')));
-                    const arrayYears = [];
-                    years.map((year) => {
-                        // Returning an object so the checkbox component gets the right shape.
-                        arrayYears.push({
-                            label: year,
-                            value: year
-                        });
-                    });
-                    this.yearsSubject.next(arrayYears);
-                }),
-                take(1)
-            )
-            .subscribe();
-    }
+    this.documentService.searchTerm$
+      .pipe(take(1))
+      .subscribe(searchTerm =>
+        this.documentService.filteredDocumentsSubject.next(this.filterEvents())
+      );
+    this.documentService.currentSelectedMonths$
+      .pipe(take(1))
+      .subscribe(months =>
+        this.documentService.filteredDocumentsSubject.next(this.filterEvents())
+      );
+    this.documentService.currentSelectedYears$
+      .pipe(take(1))
+      .subscribe(years =>
+        this.documentService.filteredDocumentsSubject.next(this.filterEvents())
+      );
+  }
 
-    setTitleFilter(searchTerm: string) {
-        this.documentService.filterDocuments(searchTerm);
-    }
+  getYearsFromResults(data: object[]) {
+    of(data)
+      .pipe(
+        map(files => {
+          const years = _.compact(_.uniq(_.map(files, 'year')));
+          const arrayYears = [];
+          years.map(year => {
+            // Returning an object so the checkbox component gets the right shape.
+            arrayYears.push({
+              label: year,
+              value: year
+            });
+          });
+          this.yearsSubject.next(arrayYears);
+        }),
+        take(1)
+      )
+      .subscribe();
+  }
 
-    setMonthFilter(month: string) {
-        this.documentService.filterDocuments(month);
-    }
+  setTitleFilter(searchTerm: string) {
+    this.documentService.setSearchTermSubject(searchTerm);
+  }
 
-    setYearFilter(year: string) {
-        this.documentService.filterDocuments(year);
-    }
+  setMonthFilter(months: string[]) {
+    this.documentService.setMonthsSubject(months);
+  }
+
+  setYearFilter(year: string[]) {
+    this.documentService.setYearsSubject(year);
+  }
+
+  filterEvents() {
+    return this.documentService.filterDocuments(
+      this.documentService.documentSubject.getValue(),
+      this.documentService.searchTermSubject.getValue(),
+      this.documentService.currentSelectedMonthsSubject.getValue(),
+      this.documentService.currentSelectedYearsSubject.getValue()
+    );
+  }
 }
--- src/app/shared/services/doc-store.service.ts
+++ src/app/shared/services/doc-store.service.ts
@@ -7,68 +7,133 @@ import { ActivatedRoute, Params } from '@angular/router';
 import * as _ from 'lodash';
 
 export interface PDF {
-    showMessage: boolean;
-    filePath: string;
-    fileSize: string;
-    label: string;
-    errorMessage: string;
-    month: string;
-    monthLabel: string;
-    year: string;
+  showMessage: boolean;
+  filePath: string;
+  fileSize: string;
+  label: string;
+  errorMessage: string;
+  month: string;
+  monthLabel: string;
+  year: string;
 }
 
 @Injectable()
 export class DocStoreService {
-    documentSubject: BehaviorSubject<PDF[]> = new BehaviorSubject([
-        {
-            showMessage: false,
-            filePath: '',
-            fileSize: '',
-            label: '',
-            errorMessage: '',
-            month: '',
-            monthLabel: '',
-            year: ''
-        }
-    ]);
+  documentSubject: BehaviorSubject<PDF[]> = new BehaviorSubject([
+    {
+      showMessage: false,
+      filePath: '',
+      fileSize: '',
+      label: '',
+      errorMessage: '',
+      month: '',
+      monthLabel: '',
+      year: ''
+    }
+  ]);
 
-    public documents$: Observable<PDF[]> = this.documentSubject.asObservable();
+  public documents$: Observable<PDF[]> = this.documentSubject.asObservable();
 
-    filteredDocumentsSubject = new BehaviorSubject<PDF[]>(null);
-    filteredDocuments$ = this.filteredDocumentsSubject.asObservable();
+  filteredDocumentsSubject = new BehaviorSubject<PDF[]>(null);
+  filteredDocuments$ = this.filteredDocumentsSubject.asObservable();
 
-    currentSelectedYearsSubject = new BehaviorSubject([]);
-    currentSelectedYears$ = this.currentSelectedYearsSubject.asObservable();
+  currentSelectedYearsSubject = new BehaviorSubject([]);
+  currentSelectedYears$ = this.currentSelectedYearsSubject.asObservable();
 
-    currentSelectedMonthsSubject = new BehaviorSubject([]);
-    currentSelectedMonths$ = this.currentSelectedMonthsSubject.asObservable();
+  currentSelectedMonthsSubject = new BehaviorSubject([]);
+  currentSelectedMonths$ = this.currentSelectedMonthsSubject.asObservable();
 
-    constructor(private http: HttpClient, private route: ActivatedRoute) {
-        this.requestPdfs();
+  searchTermSubject = new BehaviorSubject('');
+  searchTerm$ = this.searchTermSubject.asObservable();
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.requestPdfs();
+  }
+
+  requestPdfs() {
+    this.route.queryParams
+      .pipe(
+        switchMap((params: Params) =>
+          this.http.get(environment.mayorUrl, { params: params })
+        ),
+        tap((file: any) => this.documentSubject.next(file)),
+        tap(() =>
+          this.filteredDocumentsSubject.next(
+            this.filterDocumentsByTitle(this.documentSubject.getValue(), '')
+          )
+        ),
+        take(1)
+      )
+      .subscribe();
+  }
+
+  setMonthsSubject(months: string[]) {
+    this.currentSelectedMonthsSubject.next(months);
+  }
+
+  setYearsSubject(years: string[]) {
+    this.currentSelectedYearsSubject.next(years);
+  }
+
+  setSearchTermSubject(searchTerm: string) {
+    this.searchTermSubject.next(searchTerm);
+  }
+
+  filterPDFs() {
+    const documents = this.filterDocuments(
+      this.documentSubject.getValue(),
+      this.searchTermSubject.getValue(),
+      this.currentSelectedMonthsSubject.getValue(),
+      this.currentSelectedYearsSubject.getValue()
+    );
+
+    this.filteredDocumentsSubject.next(documents);
+  }
+
+  filterDocuments(
+    documents: PDF[],
+    searchTerm?: string,
+    months?: string[],
+    years?: string[]
+  ) {
+    return this.filterDocumentsByTitle(
+      this.filterDocumentsByMonth(
+        this.filterDocumentsByYear(documents, years),
+        months
+      ),
+      searchTerm
+    );
+  }
+
+  filterDocumentsByTitle(documents: PDF[], searchTerm?: string) {
+    if (_.isEmpty(searchTerm)) {
+      return documents;
     }
 
-    requestPdfs() {
-        this.route.queryParams
-            .pipe(
-                switchMap((params: Params) => this.http.get(environment.mayorUrl, { params: params })),
-                tap((file: any) => this.documentSubject.next(file)),
-                tap(() => this.filterDocuments('')),
-                take(1)
-            )
-            .subscribe();
+    return _.filter(documents, (document: PDF) => {
+      return new RegExp(searchTerm, 'i').test(document.label);
+    });
+  }
+
+  filterDocumentsByMonth(documents: PDF[], months?: string[]) {
+    if (months.length === 0) {
+      return documents;
     }
 
-    filterDocuments(searchTerm: string) {
-        const documents = this.documentSubject.getValue();
+    // filter by intersecting months.
+    return _.filter(documents, (document: PDF) => {
+      return !_.isEmpty(_.intersection(months, document.label));
+    });
+  }
 
-        if (_.isEmpty(searchTerm)) {
-            this.filteredDocumentsSubject.next(documents);
-        }
-
-        this.filteredDocumentsSubject.next(
-            _.filter(documents, (document: PDF) => {
-                return new RegExp(searchTerm, 'i').test(document.label);
-            })
-        );
+  filterDocumentsByYear(documents: PDF[], years?: string[]) {
+    if (years.length === 0) {
+      return documents;
     }
+
+    // filter by intersecting years.
+    return _.filter(documents, (document: PDF) => {
+      return !_.isEmpty(_.intersection(years, document.label));
+    });
+  }
 }
