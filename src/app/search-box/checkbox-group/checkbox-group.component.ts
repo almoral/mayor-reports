c6e389f4772835fcad0b025e2db1a983a2b2b61c
Alex Morales <almoral>
2019-04-11 09:03:37 -0400

    Removed comments from the checkbox group component.

--- src/app/search-box/checkbox-group/checkbox-group.component.ts
+++ src/app/search-box/checkbox-group/checkbox-group.component.ts
@@ -2,32 +2,31 @@ import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import * as _ from 'lodash';
 
 @Component({
-    selector: 'mdc-checkbox-group',
-    templateUrl: 'checkbox-group.component.html',
-    styleUrls: [ 'checkbox-group.component.css' ]
+  selector: 'mdc-checkbox-group',
+  templateUrl: 'checkbox-group.component.html',
+  styleUrls: ['checkbox-group.component.css']
 })
 export class CheckboxGroupComponent implements OnInit {
-    @Input() options: Array<Object>;
+  @Input() options: Array<Object>;
 
-    @Input() currentSelectedOptions: Array<string>;
+  @Input() currentSelectedOptions: Array<string>;
 
-    @Output() optionsSelected = new EventEmitter();
+  @Output() optionsSelected = new EventEmitter();
 
-    constructor() {}
+  constructor() {}
 
-    ngOnInit() {}
+  ngOnInit() {}
 
-    selectOption(event: any) {
-        console.log('selected option: ', this.currentSelectedOptions);
-
-        if (this.currentSelectedOptions.indexOf(event.target.value) !== -1) {
-            _.remove(this.currentSelectedOptions, (selectedOption) => selectedOption === event.target.value);
-            this.optionsSelected.emit(this.currentSelectedOptions);
-        } else {
-            this.currentSelectedOptions.push(event.target.value);
-            this.optionsSelected.emit(this.currentSelectedOptions);
-
-            console.log('selected options: ', this.currentSelectedOptions);
-        }
+  selectOption(event: any) {
+    if (this.currentSelectedOptions.indexOf(event.target.value) !== -1) {
+      _.remove(
+        this.currentSelectedOptions,
+        selectedOption => selectedOption === event.target.value
+      );
+      this.optionsSelected.emit(this.currentSelectedOptions);
+    } else {
+      this.currentSelectedOptions.push(event.target.value);
+      this.optionsSelected.emit(this.currentSelectedOptions);
     }
+  }
 }
