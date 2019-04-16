1ea41663137c20c23eeafc95a03609918407738b
Alex Morales <almoral>
2019-04-16 14:24:00 -0400

    WIP: Attaching this from a different angle.

--- src/app/mayor-pdf-search/mayor-pdf-search.component.ts
+++ src/app/mayor-pdf-search/mayor-pdf-search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DocStoreService } from '../shared/services/doc-store.service';
-import { Observable, of, from } from 'rxjs';
-import { tap, distinctUntilChanged, map, filter, reduce } from 'rxjs/operators';
+import { Observable, of, from, BehaviorSubject } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { MonthService } from '../shared/services/month.service';
 import * as _ from 'lodash';
@@ -13,7 +13,11 @@ import * as _ from 'lodash';
 })
 export class MayorPdfSearchComponent implements OnInit {
   files$: Observable<object>;
-  years$: Observable<string[]>;
+  yearsSubject = new BehaviorSubject<object[]>([]);
+  years$ = this.yearsSubject.asObservable();
+
+  currentSelectedYears$: Observable<string[]>;
+  currentSelectedMonths$: Observable<string[]>;
 
   months = this.monthService.months;
 
@@ -26,22 +30,40 @@ export class MayorPdfSearchComponent implements OnInit {
   ngOnInit() {
     this.ngxService.start();
 
+    this.currentSelectedYears$ = this.documentService.currentSelectedYears$;
+    this.currentSelectedMonths$ = this.documentService.currentSelectedMonths$;
+
     this.files$ = this.documentService.filteredDocuments$.pipe(
       tap((data: object[]) => {
         if (!_.isNil(data) && data.length > 0) {
           this.ngxService.stop();
-          this.years$ = this.getYearsFromResults(data);
+          this.getYearsFromResults(data);
         }
       })
     );
   }
 
-  getYearsFromResults(data: object[]): Observable<Array<string>> {
-    return of(data).pipe(
-      map(files => {
-        return _.compact(_.uniq(_.map(files, 'year')));
-      })
-    );
+  getYearsFromResults(data: object[]) {
+    of(data)
+      .pipe(
+        map(files => {
+          const years = _.compact(_.uniq(_.map(files, 'year')));
+          const arrayYears = [];
+          years.map(year => {
+            // Returning an object so the checkbox component gets the right shape.
+            arrayYears.push({
+              label: year,
+              value: year,
+              isDisabled: false
+            });
+          });
+          if (arrayYears.length > 0) {
+            this.yearsSubject.next(arrayYears);
+          }
+        }),
+        take(1)
+      )
+      .subscribe();
   }
 
   setTitleFilter(searchTerm: string) {
